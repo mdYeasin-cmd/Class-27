205a89884d15708a08e4816cd8018078ba2c88e7
mdYeasin-cmd <85502720+mdYeasin-cmd@users.noreply.github.com>
2022-06-21 15:36:18 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,37 @@
+$(function(){
+
+    // Main Menu Collapse JavaScript Start
+    $('#nav_menu_toggler').on('click', function(){
+        $('.logo').toggleClass('active');
+        $('.nav_item_contianer').toggleClass('active');
+    })
+    // //Main Menu Collapse JavaScript End
+
+    // Slick Activation JavaScript Start
+    $('.banner_item_wrapper').slick({
+        prevArrow: $('#prev_arrow'),
+        nextArrow: $('#next_arrow')
+    });
+    $('.service_item_container').slick({
+        slidesToShow: 3,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        speed: 500,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+    // //Slick Activation JavaScript End
+})
